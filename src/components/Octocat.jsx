e80c8e594932a98e3135d06c65fb93f1ef74e779
Nathan Phame <diyf2021@gmail.com>
2021-05-25 11:57:24 -0400

    Fixing up css problems. Flex wrap still not working

--- src/components/Octocat.jsx
+++ src/components/Octocat.jsx
@@ -3,34 +3,38 @@ import React, { Component } from 'react'
 class Octocat extends Component {
   render() {
     return (
-      <article>
-        <a href={this.props.link}>
-          <img
-            src={this.props.image}
-            width="400"
-            height="400"
-            alt="Terracottocat"
-          />
-        </a>
-        <ul>
-          <li>
-            #{this.props.id}:
-            <a href="https://octodex.github.com//terracottocat/">
-              <strong>{this.props.name}</strong>
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/chubbmo">
+      <main>
+        <section>
+          <article>
+            <a href={this.props.link}>
               <img
-                src={this.props.icon}
-                width="24px"
-                height="24px"
-                alt="chubbmo"
+                src={this.props.image}
+                width="400"
+                height="400"
+                alt="Terracottocat"
               />
             </a>
-          </li>
-        </ul>
-      </article>
+            <ul>
+              <li>
+                #{this.props.id}:
+                <a href="https://octodex.github.com//terracottocat/">
+                  <strong>{this.props.name}</strong>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/chubbmo">
+                  <img
+                    src={this.props.icon}
+                    width="24px"
+                    height="24px"
+                    alt="chubbmo"
+                  />
+                </a>
+              </li>
+            </ul>
+          </article>
+        </section>
+      </main>
     )
   }
 }
